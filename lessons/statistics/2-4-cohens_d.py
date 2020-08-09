8b939b3aa5a8bd11c32cc4c2707ff5897ba04924
twdooley <62968207+twdooley@users.noreply.github.com>
2020-08-09 12:19:39 -0400

    Update and rename 2-4-cohens_d.md to 2-4-cohens_d.py

--- lessons/statistics/2-4-cohens_d.py
+++ lessons/statistics/2-4-cohens_d.py
@@ -1,4 +1,3 @@
-[Think Stats Chapter 2 Exercise 4](http://greenteapress.com/thinkstats2/html/thinkstats2003.html#toc24) (Cohen's d)
 
 def WeightDiff(live, firsts, others):
     mean_live = live.totalwgt_lb.mean()
